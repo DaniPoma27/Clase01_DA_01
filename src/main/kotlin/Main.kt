8a21eb7cadce244889562871a75596f1e444f013
DaniPoma27 <103981258+DaniPoma27@users.noreply.github.com>
2022-05-18 21:46:27 -0500

    [feature] -

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.*
+
 fun main(){
     //Boolean size 1bit false/true
     //Byte size 8bit -127 to -128
@@ -18,4 +20,70 @@ fun main(){
     name = "Pedro"
     println("My name is $name $lastName")
 
+    //suma
+    println(2+2)
+    //resta
+    println(2-2)
+    //división
+    println(2/2)
+    //modulos
+    println(2%2)
+    //multiplicación
+    println(2*2)
+    //Operacion Matematica
+    println(1 + (9 / 3) * 2)
+    //potencia
+    println(2.0.pow(2))
+
+    //Comparacion e igualdad operadores
+    //igualdad
+    println(2==2)
+    //Desigualdad / Diferencia
+    println(2!=2)
+    //mayor que
+    println(2>2)
+    //menor que
+    println(2<2)
+    //mayor o igual que
+    println(2>=2)
+    //menor o igual que
+    println(2<=2)
+
+    var anyNumber: Int = 0
+
+    anyNumber += 10
+    anyNumber -= 2
+    anyNumber *= 2
+    anyNumber /= 2
+    anyNumber %= 2
+    anyNumber++
+    anyNumber--
+    println(anyNumber)
+
+    var isTrue: Boolean = true
+    var isFalse: Boolean = false
+    //Operadores Booleanos logicos
+    //AND
+    println(isTrue && isFalse)
+    //OR
+    println(isTrue || isFalse)
+    //NOT
+    println(!isTrue)
+    helloWorld()
+    var myNumber: Int = dobleTheValue(2)
+    println(myNumber)
+    favoriteFoodPrompot("Daniel", "Sushi")
+    favoriteFoodPrompot()
+}
+
+fun helloWorld(){
+    println("Hello World")
+}
+
+fun dobleTheValue(numberValue: Int): Int{
+    return numberValue * 2
+}
+
+fun favoriteFoodPrompot(name: String = "Anonymus", favoriteFood: String = "Pizza"): Unit {
+    println("$name s favorite food is $favoriteFood")
 }
